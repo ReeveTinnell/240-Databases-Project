8686498c1dbefc6cdecd84b8bab896cac93c2dbd
Reeve Tinnell <reevetinnell@gmail.com>
2025-10-11 08:51:07 -0600

    Fixed python syntax

--- companies.py
+++ companies.py
@@ -16,7 +16,7 @@ def getConnection():
 def showCompanies():
     connection = getConnection()
     mycursor = connection.cursor()
-    mycursor.execute("SELECT * FROM company;")
+    mycursor.execute("SELECT * FROM company")
     myresult = mycursor.fetchone()
     print("In the company table, we have the following items: ")
     while myresult is not None:
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     while menuOption != 'q':
         menuOption = input(menuText)
         if menuOption == "1":
-            showCompanies
+            showCompanies()
         elif menuOption == "2":
             addCompany()
         elif menuOption == "3":
