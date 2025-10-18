b0dc5b3ac16670fef18ad06f2c838b39ccbe01c5
Reeve Tinnell <reevetinnell@gmail.com>
2025-10-17 20:55:16 -0600

    further testing of menu functions

--- menu_testing.py
+++ menu_testing.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-#import mysql.connector, os
+import mysql.connector, os
+from dotenv import load_dotenv
+load_dotenv()
+
+def getConnection():
+    connection = mysql.connector.connect(
+        host=os.getenv('SQL_HOST'),
+        user=os.getenv('SQL_USER'),
+        password=os.getenv('SQL_PWD'),
+        db=os.getenv('SQL_DB')
+    )
+    return connection
 
 """ company = {name, industry, website, location, size}
 contact = {name, email, phone, position}
@@ -12,7 +23,19 @@ certs = {name, cert_body, cost, requirements}
 jobCerts = {job.id, cert.id}
 jobRole = {title, avgWage, description} """
 
-tables = {"certification", "company", "contact", "contract", "full_time", "job", "job_certs", "job_role", "part_time"}
+#tables = {"certification", "company", "contact", "contract", "full_time", "job", "job_certs", "job_role", "part_time"}
+
+tables = {}
+
+def getTables():
+    connection = getConnection()
+    myCursor = connection.cursor()
+    myCursor.execute("SHOW TABLES")
+    myResult = myCursor.fethone()
+    while myResult is not None:
+        tables.append(myResult)
+        myReslut = myCursor.fetchone()
+    connection.close()
 
 def showTables():
     number=1
