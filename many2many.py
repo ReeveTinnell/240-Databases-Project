e2718ddc0314923c6ba26f39ebb04c58bb1209b6
Reeve Tinnell <reevetinnell@gmail.com>
2025-10-18 13:52:01 -0600

    Fixing minor formatting issues

--- many2many.py
+++ many2many.py
@@ -41,7 +41,7 @@ def printCerts():
 def printCertsForJob():
     connection = getConnection()
     myCursor = connection.cursor()
-    job_id = input("For which job_id would you like to view the certifications? #>")
+    job_id = input("For which job_id would you like to view the certifications? #> ")
     myCursor.execute("SELECT certification.id, job.title, certification.name FROM job_certs JOIN job ON job_certs.job_id=job.id JOIN certification ON job_certs.cert_id=certification.id WHERE job_id=%s", (job_id,))
     myResult = myCursor.fetchall()
     print(f"There are {len(myResult)} certifications: ")
@@ -52,7 +52,7 @@ def printCertsForJob():
 def printJobsWithCert():
     connection = getConnection()
     myCursor = connection.cursor()
-    cert_id = input("For which cert_id would you like to view the jobs? #>")
+    cert_id = input("For which cert_id would you like to view the jobs? #> )
     myCursor.execute("SELECT job.id, job.title, certification.name FROM job_certs JOIN job ON job_certs.job_id=job.id JOIN certification ON job_certs.cert_id=certification.id WHERE cert_id=%s", (cert_id,))
     myResult = myCursor.fetchall()
     print(f"There are {len(myResult)} jobs: ")
@@ -67,12 +67,12 @@ def addCert():
     print(" ")
     connection = getConnection()
     myCursor = connection.cursor()
-    addToJob = input("Please provide the job.id you would like to add a certification to #>")
-    addCert = input("What certification (cert.id) would you like to add to this job? #>")
+    addToJob = input("Please provide the job.id you would like to add a certification to #> ")
+    addCert = input("What certification (cert.id) would you like to add to this job? #> ")
     query = "INSERT INTO job_certs (job_id, cert_id) VALUES (%s, %s)"
     myCursor.execute(query, (addToJob, addCert))
-    connection.commit
-    connection.close
+    connection.commit()
+    connection.close()
 
     
 
